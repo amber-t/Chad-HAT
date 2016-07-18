e1c98c65b49478fdd6260be5d7cdb2294bb4ae03
amber-t <amber.tang@yale.edu>
2016-07-18 13:04:25 -0400

    Add code

---
+++ HAT.m
@@ -0,0 +1,48 @@
+%% HAT: SEIR model with vectored transmission
+function dX=HAT(t,X,delta,a,betaVH,betaH,H,muH,taoH,gammaH1,epi,zeta,...
+    gammaH2,p,Bv,eta,sigmaV,taoV,rho,muV0,muV1,V,betaV)
+%% parameters
+Hs=X(1);
+He=X(2);
+Hi1=X(3);
+Hi2=X(4);
+Hr=X(5);
+
+Vp=X(6);
+Vs=X(7);
+Ve=X(8);
+Vi=X(9);
+Vr=X(10);
+
+Bh = muH*H + (1-rho)*gammaH2*Hi2 + rho*(1-epi)*p*zeta*Hi2; %human birth rate
+lambdaVH = betaV*betaVH*(Hi1/H); %prob of a suscpetible tsetse become infected from a human blood meal
+muV = muV0*(1 + muV1*V); %tsetse death rate
+
+dX=zeros(10,1);
+
+%% differential equations
+%humans
+dHs = Bh + delta*Hr - a*betaVH*betaH*Vi*(Hs/H) - muH*Hs;
+dHe = a*betaVH*betaH*Vi*(Hs/H) - taoH*He - muH*He;
+dHi1 = taoH*He - gammaH1*Hi1 - muH*Hi1;
+dHi2 = gammaH1*Hi1 - rho*epi*zeta*Hi2 - (1 - rho)*gammaH2*Hi2 - rho*(1-epi)*p*zeta*Hi2 - muH*Hi2;
+dHr = rho*epi*zeta*Hi2 - delta*Hr - muH*Hr;
+
+%tsetse
+dVp = Bv*V + eta*Vp;
+dVs= eta*Vp - a*Vs - sigmaV*Vs - muV*Vs;
+dVe = a*lambdaVH *Vs - taoV*Ve - muV*Ve;
+dVi = taoV*Ve - muV*Vi;
+dVr = a*(1-lambdaVH)*Vs + sigmaV*Vs - muV*Vr;
+
+dX(1)=dHs;
+dX(2)=dHe;
+dX(3)=dHi1;
+dX(4)=dHi2;
+dX(5)=dHr;
+
+dX(6)=dVp;
+dX(7)=dVs;
+dX(8)=dVe;
+dX(9)=dVi;
+dX(10)=dVr;
---
+++ HATrun.m
@@ -0,0 +1,59 @@
+%% initial conditions
+%humans
+X_0=zeros(10,1);
+X_0(1)=0.75; %suscep
+X_0(2)=0.2; %exposed
+X_0(3)=0.05; %infectious s1
+X_0(4)=0; %infectious s2
+X_0(5)=0; %recovered
+
+%tsetse
+X_0(6)=0.1; %pupae stage
+X_0(7)=0.5; %susceptible
+X_0(8)=0.1; %exposed
+X_0(9)=0; %infected
+X_0(10)=0.3; %recovered
+
+%% paramters (days)
+%humans
+H=14500; %human population size
+muH=4.66e-5; %human death rate
+betaH=0.1751; %transmission prob from tsetse > human
+taoH=1/12; %human incubation period
+gammaH1=1/526; %stage 1 infectious period 
+gammaH2=1/252; %stage 2 infectious period
+
+%tsetse
+V=17*14500; %tsetse population size
+eta=1/20; %pupae stage duration
+Bv=0.05; %tsetse birth rate 
+muV0=0.030; %tsetse death rate no comp
+muV1=0.0002; %tsetse death comp param
+sigmaV=1; %susceptibility period
+a=0.333; %human bite rate
+betaVH=0.3750; %proportion of tsetse bites on humans
+taoV=1/25; %incubation period in tsetse
+betaV=0.2; %transmission prob from humans to tsetse
+
+%treatment
+epi=0.965; %efficiency of stage II treatment (nifurtimox-eflornithine)
+zeta=1/202; %treatment seeking rate for stage II patietns
+p=0.007; %probability of death due to stage 2 treatment failure
+delta=1/50; %immune period in humans after treatment
+rho=0.87; %sensitivity of CATT-TL
+
+%% ode45
+[t,X]=ode45(@HAT,[0:1:500],X_0,[],delta,a,betaVH,betaH,H,muH,taoH,gammaH1,epi,zeta,...
+    gammaH2,p,Bv,eta,sigmaV,taoV,rho,muV0,muV1,V,betaV);
+
+Hs=X(:,1);
+He=X(:,2);
+Hi1=X(:,3);
+Hi2=X(:,4);
+Hr=X(:,5);
+
+Vp=X(:,6);
+Vs=X(:,7);
+Ve=X(:,8);
+Vi=X(:,9);
+Vr=X(:,10);
