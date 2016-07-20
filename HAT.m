96ab9f328b47e23f22aafe369f1bea8d843dc650
amber-t <amber.tang@yale.edu>
2016-07-20 14:02:58 -0400

    fixed code

--- HAT.m
+++ HAT.m
@@ -1,17 +1,17 @@
 %% HAT: SEIR model with vectored transmission
-function dX=HAT(t,X,delta,a,betaVH,betaH,betaV,muH,taoH,gammaH1,epi1,epi2,zeta1,zeta2,...
-    gammaH2,p2,Bv,eta,sigmaV,taoV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP)
+function dX=HAT(t,X,deltaH,aH,betaVH,betaH,muH,tauH,gammaH1,eps1,eps2,zeta1,zeta2,...
+    gammaH2,p2,BV,eta,sigmaV,tauV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP,betaV)
 %% parameters
 Hs=X(1); He=X(2); Hi1=X(3); Hi2=X(4); Hr=X(5); 
 Vp=X(6); Vs=X(7); Ve=X(8); Vi=X(9); Vr=X(10);
 
 H=Hs+He+Hi1+Hi2+Hr;
-V=Vp+Vs+Ve+Vi+Vr;
+V=Vs+Ve+Vi+Vr;
 
 phi1=P1*P1PD*P1TP; %coverage stage 1 
 phi2=P2*P2PD*P2TP; %coversage stage 2
 
-Bh = muH*H + (1-phi2)*gammaH2*Hi2 + phi2*(1-epi2)*p2*zeta2*Hi2; %human birth rate
+Bh = muH*H + (1-phi2)*gammaH2*Hi2 + phi2*(1-eps2)*p2*zeta2*Hi2; %human birth rate
 lambdaVH = betaV*betaVH*(Hi1/H); %prob of a suscpetible tsetse become infected from a human blood meal
 muV = muV0*(1 + muV1*V); %tsetse death rate
 
@@ -20,27 +20,17 @@ dX=zeros(10,1);
 
 %% differential equations
 %humans
-dHs = Bh + delta*Hr - a*betaVH*betaH*Vi*(Hs/H) - muH*Hs;
-dHe = a*betaVH*betaH*Vi*(Hs/H) - taoH*He - muH*He;
-dHi1 = taoH*He - phi1*epi1*zeta1*Hi1 - (1-phi1)*gammaH1*Hi1 - muH*Hi1;
-dHi2 = (1-phi1)*gammaH1*Hi1 + - phi2*epi2*zeta2*Hi2 - (1 - phi2)*gammaH2*Hi2 - phi2*(1-epi2)*p2*zeta2*Hi2 - muH*Hi2;
-dHr = phi1*epi1*zeta1*Hi1 + phi2*epi2*zeta2*Hi2 - delta*Hr - muH*Hr;
+dHs = Bh + deltaH*Hr - aH*betaVH*betaH*Vi*(Hs/H) - muH*Hs;
+dHe = aH*betaVH*betaH*Vi*(Hs/H) - tauH*He - muH*He;
+dHi1 = tauH*He - phi1*eps1*zeta1*Hi1 - (1-phi1)*gammaH1*Hi1 - muH*Hi1;
+dHi2 = (1-phi1)*gammaH1*Hi1 - phi2*eps2*zeta2*Hi2 - (1 - phi2)*gammaH2*Hi2 - phi2*(1-eps2)*p2*zeta2*Hi2 - muH*Hi2;
+dHr = phi1*eps1*zeta1*Hi1 + phi2*eps2*zeta2*Hi2 - deltaH*Hr - muH*Hr;
 
 %tsetse
-dVp = Bv*V + eta*Vp;
-dVs= eta*Vp - a*Vs - sigmaV*Vs - muV*Vs;
-dVe = a*lambdaVH *Vs - taoV*Ve - muV*Ve;
-dVi = taoV*Ve - muV*Vi;
-dVr = a*(1-lambdaVH)*Vs + sigmaV*Vs - muV*Vr;
+dVp = BV*V - eta*Vp;
+dVs= eta*Vp - aH*Vs - sigmaV*Vs - muV*Vs;
+dVe = aH*lambdaVH *Vs - tauV*Ve - muV*Ve;
+dVi = tauV*Ve - muV*Vi;
+dVr = aH*(1-lambdaVH)*Vs + sigmaV*Vs - muV*Vr;
 
-dX(1)=dHs;
-dX(2)=dHe;
-dX(3)=dHi1;
-dX(4)=dHi2;
-dX(5)=dHr;
-
-dX(6)=dVp;
-dX(7)=dVs;
-dX(8)=dVe;
-dX(9)=dVi;
-dX(10)=dVr;
+dX = vertcat(dHs,dHe,dHi1,dHi2,dHr,dVp,dVs,dVe,dVi,dVr);
--- HATrun.m
+++ HATrun.m
@@ -1,23 +1,24 @@
-%% paramters (days)
+%% parameters (days)
+
 %humans
 H=300;
-muH=4.66e-5; %human death rate
+muH=1/59; %4.66e-5; %human death rate
 betaH=0.1751; %transmission prob from tsetse > human
-taoH=1/12; %human incubation period
-gammaH1=1/526; %stage 1 infectious period
-gammaH2=1/252; %stage 2 infectious period
+tauH=365/12; %human incubation period
+gammaH1=365/526; %stage 1 infectious period
+gammaH2=365/252; %stage 2 infectious period
 
 %tsetse
 V=5000;
-eta=1/20; %pupae stage duration
-Bv=0.05; %tsetse birth rate
-muV0=0.030; %tsetse death rate no comp
+eta=365/20; %pupae stage duration
+BV=365*0.05; %tsetse birth rate
+muV0=365*0.030; %tsetse death rate no comp
 muV1=0.0002; %tsetse death comp param
-sigmaV=1; %susceptibility period
-a=1/3; %human bite rate
+sigmaV=365; %susceptibility period
+aH=365/3; %human bite rate
 betaVH=0.3750; %proportion of tsetse bites on humans
-taoV=1/25; %incubation period in tsetse
-betaV=0.2; %transmission prob from humans to tsetse
+tauV=365/25; %incubation period in tsetse
+betaV=0.2;
 
 %treatment
 P1=0; %prob of a stage I individual getting a CATT test
@@ -26,35 +27,30 @@ P1TP=1; %prob of treatment after testing +ve for stage I
 P2=1; %prob of stage II individual getting a CATT test
 P2PD=0.87; %sensitivity of test for stage II
 P2TP=1; %prob of treatment after testing +ve for stage II
-epi1=0.94; %efficiency of stage I treatment 
-epi2=0.965; %efficiency of stage II treatment (nifurtimox-eflornithine)
+eps1=0.94; %efficiency of stage I treatment 
+eps2=0.965; %efficiency of stage II treatment (nifurtimox-eflornithine)
 zeta1=1; %treatment seeking rate for stage II patients
 zeta2=1/202; %treatment seeking rate for stage II patients
 p2=0.007; %probability of death due to stage 2 treatment failure
-delta=1/50; %immune period in humans after treatment
-rho=0.87; %sensitivity of CATT-TL
-
+deltaH=365/50; %immune period in humans after treatment
 
 %% initial conditions
 %humans
-h_0=[0.85,0.1,0.5,0,0]; %[suscep,exposed,s1,s2,recovered]
-h_0= H.*h_0;
-
+h_0=[H,0,0,0,0]; %[suscep,exposed,s1,s2,recovered]
 %tsetse
-t_0=[0.1,0.7,0.1,0,0.1]; %pupae,suscep,exposed,infected,recovered]
-t_0=V.*t_0;
+v_0=[BV*V/eta,0.99*V, 0, 0.01*V,0]; %pupae,suscep,exposed,infected,recovered]
 
-X_0=horzcat(h_0,t_0);
+X_0=horzcat(h_0,v_0);
 
 %% ode45
-[t,X]=ode15s(@HAT,[0:365:1000*365],X_0,[],delta,a,betaVH,betaH,muH,taoH,gammaH1,epi1,epi2,zeta1,zeta2,...
-    gammaH2,p2,Bv,eta,sigmaV,taoV,muV0,muV1,betaV,P1,P1PD,P1TP,P2,P2PD,P2TP);
+[t,X]=ode23s(@HAT,[0:1000],X_0,[],deltaH,aH,betaVH,betaH,muH,tauH,gammaH1,eps1,eps2,zeta1,zeta2,...
+    gammaH2,p2,BV,eta,sigmaV,tauV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP,betaV);
 
 %% equilibrium prevalences (2002)
 
 S1(1)=(X(end,3))/sum(X(end,1:5)); %2002 s1
 S2(1)=(X(end,4))/sum(X(end,1:5)); %2002 s2
-V=(X(end,9))/sum(X(end,6:10)); %2002, Vi
+V(1)=(X(end,9))/sum(X(end,6:10)); %2002 Vi
 
 X01=X(end,:); %new intiials
 cov=0.66203*0.87; %coverage=attendance*sensitivity
@@ -62,14 +58,15 @@ X01(5)=X01(5)+cov*(X01(3)+X01(4)); %new recovered after active surveillance
 X01(3)=(1-cov)*X01(3); %new s1 are those who weren't covered in surveillance
 X01(4)=(1-cov)*X01(4); %new s2
 
-tspan1=linspace(0,2*365,3); % 2002-2004
-[t1,X1]=ode15s(@HAT,tspan1,X01,[],delta,a,betaVH,betaH,muH,taoH,gammaH1,epi1,epi2,zeta1,zeta2,...
-    gammaH2,p2,Bv,eta,sigmaV,taoV,muV0,muV1,betaV,P1,P1PD,P1TP,P2,P2PD,P2TP);
+tspan1=linspace(0,2,3); % 2002-2004
+[t1,X1]=ode23s(@HAT,tspan1,X01,[],deltaH,aH,betaVH,betaH,muH,tauH,gammaH1,eps1,eps2,zeta1,zeta2,...
+    gammaH2,p2,BV,eta,sigmaV,tauV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP,betaV);
 
 %% end of 2003, beg. 2004
 
 S1(2)=(X1(end,3))/sum(X1(end,1:5)); %2004 s1
 S2(2)=(X1(end,4))/sum(X1(end,1:5)); %2004 s2
+V(2)=(X1(end,9))/sum(X1(end,6:10)); %2004 Vi
 
 %2004-2006
 X02=X1(end,:); %new intiials
@@ -78,14 +75,15 @@ X02(5)=X02(5)+cov*(X02(3)+X02(4)); %new recovered after active surveillance
 X02(3)=(1-cov)*X02(3); %new s1 are those who weren't covered in surveillance
 X02(4)=(1-cov)*X02(4); %new s2
 
-tspan2=linspace(0,2*365,3); % 2004-2006
-[t2,X2]=ode15s(@HAT,tspan2,X02,[],delta,a,betaVH,betaH,muH,taoH,gammaH1,epi1,epi2,zeta1,zeta2,...
-    gammaH2,p2,Bv,eta,sigmaV,taoV,muV0,muV1,betaV,P1,P1PD,P1TP,P2,P2PD,P2TP);
+tspan2=linspace(0,2,3); % 2004-2006
+[t2,X2]=ode23s(@HAT,tspan2,X02,[],deltaH,aH,betaVH,betaH,muH,tauH,gammaH1,eps1,eps2,zeta1,zeta2,...
+    gammaH2,p2,BV,eta,sigmaV,tauV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP,betaV);
 
 %% end of 2005, beg. 2006
 
 S1(3)=(X2(end,3))/sum(X2(end,1:5)); %2006 s1
 S2(3)=(X2(end,4))/sum(X2(end,1:5)); %2006 s2
+V(3)=(X2(end,9))/sum(X2(end,6:10)); %2006 Vi
 
 %2006-2012
 X03=X2(end,:); %new intiials
@@ -94,14 +92,15 @@ X03(5)=X03(5)+cov*(X03(3)+X03(4)); %new recovered after active surveillance
 X03(3)=(1-cov)*X03(3); %new s1 are those who weren't covered in surveillance
 X03(4)=(1-cov)*X03(4); %new s2
 
-tspan3=linspace(0,6*365,7); % 2006-2012
-[t3,X3]=ode15s(@HAT,tspan3,X03,[],delta,a,betaVH,betaH,muH,taoH,gammaH1,epi1,epi2,zeta1,zeta2,...
-    gammaH2,p2,Bv,eta,sigmaV,taoV,muV0,muV1,betaV,P1,P1PD,P1TP,P2,P2PD,P2TP);
+tspan3=linspace(0,6,7); % 2006-2012
+[t3,X3]=ode23s(@HAT,tspan3,X03,[],deltaH,aH,betaVH,betaH,muH,tauH,gammaH1,eps1,eps2,zeta1,zeta2,...
+    gammaH2,p2,BV,eta,sigmaV,tauV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP,betaV);
 
 %% end of 2011; beg. 2012
 
 S1(4)=(X3(end,3))/sum(X3(end,1:5)); %2012 s1
 S2(4)=(X3(end,4))/sum(X3(end,1:5)); %2012 s2
+V(4)=(X3(end,9))/sum(X3(end,6:10)); %2012 Vi
 
 X04=X3(end,:); 
 cov=0.803716*0.87; %coverage=attendance*sensitivity
@@ -109,10 +108,22 @@ X04(5)=X04(5)+cov*(X04(3)+X03(4)); %new recovered after active surveillance
 X04(3)=(1-cov)*X04(3); %new s1 are those who weren't covered in surveillance
 X04(4)=(1-cov)*X04(4); %new s2
 
-tspan4=linspace(0,365,2); %run from end of 2011 to end of 2012 
-[t4,X4]=ode15s(@HAT,tspan4,X04,[],delta,a,betaVH,betaH,muH,taoH,gammaH1,epi1,epi2,zeta1,zeta2,...
-    gammaH2,p2,Bv,eta,sigmaV,taoV,muV0,muV1,betaV,P1,P1PD,P1TP,P2,P2PD,P2TP);
+tspan4=linspace(0,1,2); %run from end of 2011 to end of 2012 
+[t4,X4]=ode23s(@HAT,tspan4,X04,[],deltaH,aH,betaVH,betaH,muH,tauH,gammaH1,eps1,eps2,zeta1,zeta2,...
+    gammaH2,p2,BV,eta,sigmaV,tauV,muV0,muV1,P1,P1PD,P1TP,P2,P2PD,P2TP,betaV);
 
 %% 2013
 S1(5)=(X4(end,3))/sum(X4(end,1:5)); %2013
 S2(5)=(X4(end,4))/sum(X4(end,1:5)); %2013
+V(5)=(X4(end,9))/sum(X4(end,6:10)); %2013 Vi
+
+S1=S1'; S2=S2'; V=V';
+
+%S1=vertcat(X(:,3),S1);
+%S2=vertcat(X(:,4),S2);
+%V=vertcat(X(:,9),V);
+
+Inf=horzcat(S1,S2,V);
+
+xval=[0:1:4];
+plot(xval,Inf)
